2a5b89a7591f37be9b87561a8b2d76592c793079
tybo-dev <tobias@diez.guru>
2023-02-07 15:08:55 +0100

    global tests

--- global.js
+++ global.js
@@ -3,9 +3,13 @@
 //console.log(global);
 
 setTimeout(() => {
-    console.log("inside Timeout")
+    console.log("inside Timeout");
+    clearInterval(int);
 }, 1000)
 
 const int = setInterval(() => {
     console.log("inside Intervall");
 }, 100);
+
+console.log(__dirname);
+console.log(__filename);
