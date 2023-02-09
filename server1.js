7b562d719139d5a02b8c3d53f9fc73b589d38157
tybo-dev <tobias@diez.guru>
2023-02-09 22:28:36 +0100

    server write added

--- server1.js
+++ server1.js
@@ -1,21 +1,26 @@
-
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log('request made');
+  console.log("request made");
   console.log(req.url, req.method);
 
   //set header content type
-  res.setHeader('Content-Type', "text/html")
+  res.setHeader("Content-Type", "text/html");
 
-  res.write("<p>hello, Dogs</p>")
-  res.write("<p>hello, Dogs - how r u?</p>")
-  res.end();
-
-  
+  //send a html file
+  fs.readFile("./views/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      //res.write(data);
+      res.end(data);
+    }
+  });
 });
 
 // localhost is the default value for 2nd argument
-server.listen(3000, 'localhost', () => {
-  console.log('listening for requests on port 3000');
+server.listen(3000, "localhost", () => {
+  console.log("listening for requests on port 3000");
 });
