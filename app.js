16ec7d13f47741858da6ba86bc621fc68a7fbb11
tybo-dev <tobias@diez.guru>
2023-03-06 10:55:29 +0100

    connection to db added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 // express app
 const app = express();
@@ -12,14 +13,35 @@ mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
-
 // register view engine
 app.set('view engine', 'ejs');
-
 
 //middleware and static files
 app.use(express.static("public"));
 app.use(morgan("dev"));
+
+//mongoose and mongo sandbox routes
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "newest newBlog",
+    snippet: "best new blog ever",
+    body: "you are invited to read more..."
+  });
+
+  blog.save()
+  .then((result) => res.send(result))
+  .catch((err) => console.log(err));
+});
+
+app.get("/all-blogs", (req, res) => {
+  Blog.find()
+  .then((result) => {
+    res.send(result);
+  })
+  .catch((err) => console.log(err));
+})
+
+
 
 app.get('/', (req, res) => {
   const blogs = [
